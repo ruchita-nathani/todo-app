df7a8404909784ad948f3f874bd9c0af271ca800
Ruchita Nathani <ruchita.nathani@fau.de>
2024-01-25 19:16:37 +0100

    Added comments
    Signed-off-by: Ruchita Nathani <ruchita.nathani@fau.de>

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     sno =  db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -18,6 +19,12 @@ class Todo(db.Model):
 
 @app.route("/", methods=['GET', 'POST'])
 def show_todo():
+    """
+    Renders the index.html template and handles the creation of new todo items.
+
+    Returns:
+        rendered template: index.html with all_todo data
+    """
     if request.method == 'POST':
         title = request.form['title']
         decs = request.form['desc']  
@@ -29,6 +36,16 @@ def show_todo():
 
 @app.route("/update/<int:sno>", methods=['GET', 'POST'])
 def update_todo(sno):
+    """
+    Renders the update.html template and handles the updating of a specific todo item.
+
+    Args:
+        sno (int): The serial number of the todo item to be updated.
+
+    Returns:
+        rendered template: update.html with todo data
+        redirect: to the home page after updating the todo item
+    """
     if request.method == 'POST':
         title = request.form['title']
         decs = request.form['desc']  
@@ -44,6 +61,15 @@ def update_todo(sno):
 
 @app.route("/delete/<int:sno>")
 def delete_todo(sno):
+    """
+    Deletes a specific todo item.
+
+    Args:
+        sno (int): The serial number of the todo item to be deleted.
+
+    Returns:
+        redirect: to the home page after deleting the todo item
+    """
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
@@ -52,6 +78,12 @@ def delete_todo(sno):
 
 @app.route("/about")
 def about():
+    """
+    Renders the about.html template.
+
+    Returns:
+        rendered template: about.html
+    """
     return render_template('about.html')
 
 if __name__ == "__main__":
